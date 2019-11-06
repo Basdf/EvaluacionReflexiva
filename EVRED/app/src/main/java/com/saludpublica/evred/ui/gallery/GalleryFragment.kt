3b0844a3f83ee623394b8103bd6957e2b0c15810
eduardo <cancer19990706@gmail.com>
2019-11-06 16:18:44 -0500

    Encuesta estudiante V2.1 y encuesta docente en proceso

--- EVRED/app/src/main/java/com/saludpublica/evred/ui/gallery/GalleryFragment.kt
+++ EVRED/app/src/main/java/com/saludpublica/evred/ui/gallery/GalleryFragment.kt
@@ -21,11 +21,13 @@ class GalleryFragment : Fragment() {
     ): View? {
         galleryViewModel =
             ViewModelProviders.of(this).get(GalleryViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_gallery, container, false)
+        val root = inflater.inflate(R.layout.fragment_encuesta_docente, container, false)
         val textView: TextView = root.findViewById(R.id.text_gallery)
         galleryViewModel.text.observe(this, Observer {
             textView.text = it
         })
         return root
     }
+
+
 }
