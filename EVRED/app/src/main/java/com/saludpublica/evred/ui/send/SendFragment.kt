ecd85c2025e90ed24aade9dd60028d551e34f96c
eduardo <cancer19990706@gmail.com>
2019-11-25 10:39:26 -0500

    Encuesta Compromiso vista beta

--- EVRED/app/src/main/java/com/saludpublica/evred/ui/send/SendFragment.kt
+++ EVRED/app/src/main/java/com/saludpublica/evred/ui/send/SendFragment.kt
@@ -4,28 +4,36 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.*
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
 import com.saludpublica.evred.R
 
-class SendFragment : Fragment() {
 
-    private lateinit var sendViewModel: SendViewModel
+class SendFragment : Fragment() {
+    var numeroC =1
+    var textId="CompromisoNumero"
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        sendViewModel =
-            ViewModelProviders.of(this).get(SendViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_send, container, false)
-        val textView: TextView = root.findViewById(R.id.text_send)
-        sendViewModel.text.observe(this, Observer {
-            textView.text = it
-        })
+        val contexto = activity!!.applicationContext
+        val root = inflater.inflate(R.layout.fragment_encuesta_compromiso, container, false)
+        val addButton :ImageButton= root.findViewById(R.id.AddButton)
+        val ll = root.findViewById(R.id.layout) as LinearLayout
+        addButton.setOnClickListener(){
+            val params = LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.MATCH_PARENT,
+                LinearLayout.LayoutParams.WRAP_CONTENT
+            )
+            val editText = EditText(context)
+            numeroC++
+            editText.id=numeroC
+            ll.addView(editText,params)
+
+        }
         return root
     }
 }
